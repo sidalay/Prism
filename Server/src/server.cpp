e2954f4d0e1d386dfb05a9c7081543efd62e4ff7
Sid Shingaki <sidalaytv@gmail.com>
2023-03-18 21:09:02 -0700

    chore: sockets
    - create listening socket
    - bind ip & port to listening socket

--- Server/src/server.cpp
+++ Server/src/server.cpp
@@ -14,8 +14,19 @@ int main()
   }
 
   // Create a socket
+  SOCKET listening{socket(AF_INET, SOCK_STREAM, 0)};
+
+  if (listening == INVALID_SOCKET) {
+    std::cerr << "Can not create a socket! Quitting" << std::endl;
+    return 0;
+  }
 
   // Bind an ip address and port to a socket
+  sockaddr_in hint;
+  hint.sin_family = AF_INET;
+  hint.sin_port = htons(54000);
+  hint.sin_addr.S_un.S_addr = INADDR_ANY; // could also use inet_pton...
+  bind(listening, (sockaddr*)&hint, sizeof(hint));
 
   // Listen on the socket for a client
 
